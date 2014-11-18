9ceff12da706f8c5b977d1e6109df38b7d2ef152
Astropsychics <snios@phys.uconn.edu>
2014-11-18 14:17:21 -0500

    Added additional CX sources

--- Modules/cx_input.h
+++ Modules/cx_input.h
@@ -4,28 +4,40 @@
 using namespace std;
 
 //number of elements
-int input_cx_elements = 11;
+int input_cx_elements = 20;
 
 //cross section, in units of 10-17 cm^2
-double input_cx_sigma[11] = {4.16, 0.722, 9.5, 3.71, 2.27, 6.3, 5.3, 3.67, 7.2, 3.7};
+double input_cx_sigma[20] = {4.16, 0.722, 9.5, 3.71, 2.27, 6.3, 5.3, 3.67, 7.2, 3.7, 3.7,
+                            4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0};
 
 //particle density, relative to O
-//double input_cx_n[11] = {.08, .20, .017, .035, .065, .014, .08, .73, .002, .094, .098};
+//double input_cx_n[20] = {0.318, 0.210, 0.006, 0.058, 0.065, 0.070, 0.200, 0.730, 0.004,
+//                         0.084, 0.098, 0.052, 0.002, 0.007, 0.023, 0.031, 0.021, 0.005, 0.016, 0.019};
+
+
+//Increased O8+ production
+//double input_cx_n[20] = {0.318, 0.210, 0.006, 0.058, 0.065, 0.130, 0.200, 0.670, 0.004,
+//                         0.084, 0.098, 0.052, 0.002, 0.007, 0.023, 0.031, 0.021, 0.005, 0.016, 0.019};
 
 //Increased O8+ and Ne9+ production
-double input_cx_n[11] = {0.318, 0.210, 0.006, 0.058, 0.065, 0.130, 0.200, 0.670, 0.020,
-			 0.084, 0.098};
+double input_cx_n[20] = {0.318, 0.210, 0.006, 0.058, 0.065, 0.130, 0.200, 0.670, 0.020,
+                         0.084, 0.098, 0.052, 0.002, 0.007, 0.023, 0.031, 0.021, 0.005, 0.016, 0.019};
+
 
 //cx low sw speed table filename
-string input_cx_line_name_low[11] = {"C6collisionwithH2V0.19","C5collisionwithH2V0.18",
+string input_cx_line_name_low[20] = {"C6collisionwithH2V0.19","C5collisionwithH2V0.18",
     "N7collisionwithH2OV0.20","N6collisionwithH2V0.21","N5collisionwithH2V0.19",
     "myGreenO8H2O","O7collisionwithH2OgreenfitV0.20","O6collisionwithHV0.19",
     "Ne9collisionwithH2V0.20","Ne8CollisionwithH2V0.25",
-    "Mg10CollisionwithH2V0.25"};
-    
+    "Mg10CollisionwithH2V0.25","Mg9collisionwithH2","Fe13collisionwithH2",
+    "Fe12collisionwithH2","Fe11collisionwithH2","Fe10collisionwithH2",
+    "Si9collisionwithH2","S10collisionwithH2","S9collisionwithH2",
+    "S8collisionwithH2"};
+
 //cx high sw speed table filename
-string input_cx_line_name_high[11] = {"xxxxxxxxx","xxxxxxxxx","xxxxxxxxx",
-    "xxxxxxxxx","xxxxxxxxx","xxxxxxxxx","xxxxxxxxx","xxxxxxxxx",
-    "xxxxxxxxx","xxxxxxxxx","xxxxxxxxx"};
+string input_cx_line_name_high[20] = {"xxxxxxxxx","xxxxxxxxx","xxxxxxxxx",
+    "xxxxxxxxx","xxxxxxxxx","xxxxxxxxx","xxxxxxxxx","xxxxxxxxx","xxxxxxxxx",
+    "xxxxxxxxx","xxxxxxxxx","xxxxxxxxx","xxxxxxxxx","xxxxxxxxx","xxxxxxxxx",
+    "xxxxxxxxx","xxxxxxxxx","xxxxxxxxx","xxxxxxxxx","xxxxxxxxx"};
 
 #endif
