79fe35e4e10e733b8a8b380427987553c65a6e46
Bradford Snios <snios@firsov.phys.uconn.edu>
2015-03-12 10:58:30 -0400

    Updated CX Plotter

--- Plots/Plotters/Chandra_CX_Comparison.m
+++ Plots/Plotters/Chandra_CX_Comparison.m
@@ -20,14 +20,14 @@ EO = dO(:,1);
 IO = dO(:,2); 
 
 figure
-semilogy(E,I,'g', EO,IO,'ks', 'LineWidth',2.5)
+semilogy(E,I,'g',EO,IO,'ks','LineWidth',2.5)
 set(gca,'FontSize',16)
 xlim([0 1.2])
 %ylim([10^-3,1])
 %title(strcat(comet_name,' CX Comparison'))
 xlabel('Energy [keV]')
 ylabel('Counts sec^{-1} keV^{-1}');
-legend('Model','Observation','Location','NorthEast');
+legend('CX Model','Observation','Location','NorthEast');
 print('-depsc2',strcat('../Chandra_CX_comparison_',comet_name,'.eps'));
 
 clear all
